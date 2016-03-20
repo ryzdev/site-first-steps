73d0b1fb29ca02d8a87c041dbf948cb2ec49de60
Ryz Jenvey <ryszard.jenvey@autotrader.co.uk>
2016-03-20 22:57:54 +0000

    remove angular mini examples, move a directory

--- learning-angular/egghead-tutorials/1c evaluateExpression.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("evaluateExpression", []);
-
-app.controller("EvaluateExpressionController", function($scope) {
-    $scope.giveMeaningOfLife = function(){
-        return 21*2;
-    }
-})
